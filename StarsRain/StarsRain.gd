110e7810d91a8f0fd26b497a066896f47e690f05
DEVizzent <vizzent@gmail.com>
2023-11-18 07:36:26 +0100

    Create logic to spawn random stars by the player position

---
+++ StarsRain/StarsRain.gd
@@ -0,0 +1,24 @@
+extends Node
+
+@export var positionReference:Node2D
+@export var levelRange:Range
+@export var word:String
+var positionsToThrowStars:Array[int] = []
+
+func _ready():
+	var rnd = RandomNumberGenerator.new()
+	for letter in word:
+		positionsToThrowStars.append(randf_range(levelRange.min_value, levelRange.max_value))
+	positionsToThrowStars.sort_custom(compare_int)
+
+func compare_int(a:int,b:int)->bool:
+	return a < b
+
+func _process(_delta)->void:
+	if positionReference.global_position.x < positionsToThrowStars[0]:
+		return
+	#TODO: Activar star y eliminar prints
+	print("Activamos star")
+	print(positionsToThrowStars.size())
+	positionsToThrowStars.remove_at(0) 
+
