304653564c96c91ec9308f749c02f43f4b31c8a4
DEVizzent <vizzent@gmail.com>
2023-11-18 12:01:58 +0100

    Integrar settings en menu de inicio y agregar boton de cierre

--- Inicio/StartMenu.gd
+++ Inicio/StartMenu.gd
@@ -1,4 +1,17 @@
 extends Node2D
 
 func _ready():
-	$Panel/StartButton.grab_focus()
+	$Panel/CenterContainer/VBoxContainer/StartButton.grab_focus()
+
+func _on_settings_settings_closed():
+	$Panel/CenterContainer/VBoxContainer/SettingsButton.grab_focus()
+
+func _on_settings_button_pressed():
+	var menu_event = InputEventAction.new()
+	menu_event.action = "ui_menu"
+	menu_event.pressed = true
+	Input.parse_input_event(menu_event)
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
