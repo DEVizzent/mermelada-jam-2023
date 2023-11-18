322106d08c9d3cd3d428ea276731fe6c5d7bbfa8
A. Roig <antiaroig@gmail.com>
2023-11-18 19:02:00 +0100

    Animación gato incial

--- Player/Player.gd
+++ Player/Player.gd
@@ -25,11 +25,23 @@ func _player_movement()->void:
 		return
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if Input.is_action_pressed("ui_left") && is_on_floor():
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("run")
+	
+	if Input.is_action_pressed("ui_right") && is_on_floor():
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("run")
+	
+	if !Input.is_anything_pressed():
+		$AnimatedSprite2D.play("idle")
 
 func _blockPlayerMovement()->void:
 	velocity.x = 0
