a6a54191a112859ea6917d74c45ccc560b0830fc
DEVizzent <vizzent@gmail.com>
2023-11-18 19:20:19 +0100

    Fix and refactor player animation logic

--- Player/Player.gd
+++ Player/Player.gd
@@ -23,25 +23,24 @@ func _physics_process(delta)->void:
 func _player_movement()->void:
 	if isPlayerMovementBlocked:
 		return
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		$AnimatedSprite2D.play("jump")
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	if Input.is_action_pressed("ui_left") && is_on_floor():
-		$AnimatedSprite2D.flip_h = true
+	_animate_movement()
+
+func _animate_movement()->void:
+	if velocity.x != 0 && is_on_floor():
 		$AnimatedSprite2D.play("run")
-	
-	if Input.is_action_pressed("ui_right") && is_on_floor():
-		$AnimatedSprite2D.flip_h = false
-		$AnimatedSprite2D.play("run")
+	$AnimatedSprite2D.flip_h = Input.is_action_pressed("ui_left")
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
 	
 	if !Input.is_anything_pressed():
 		$AnimatedSprite2D.play("idle")
+
 
 func _blockPlayerMovement()->void:
 	velocity.x = 0
