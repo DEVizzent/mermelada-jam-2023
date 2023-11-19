e9214894c0d7754e9b0100ff9c8c4e08f4a0d7de
ferter <ferratersimon@gmail.com>
2023-11-19 12:27:29 +0100

    4 mejoras de nada

--- DeathZone/DeathZone.gd
+++ DeathZone/DeathZone.gd
@@ -1,5 +1,9 @@
 extends Area2D
-	
+
+func _process(delta):
+	var player = get_tree().get_first_node_in_group("player")
+	position.x = player.position.x
+
 func _on_body_entered(body):
 	if (body.name == "Player"):
 		get_tree().reload_current_scene()
