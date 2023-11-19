486a81ea75d837c629cede869dcf95304b3a84dc
ferter <ferratersimon@gmail.com>
2023-11-19 12:19:19 +0100

    bloque reohutilitables, ahora esta aacito el levl 1 al iniciar

--- Levels/LevelGoalArea.gd
+++ Levels/LevelGoalArea.gd
@@ -1,4 +1,5 @@
 extends Area2D
 
-func _on_body_entered(_body):
-	EventBus.emit_signal("levelCompleted")
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		EventBus.emit_signal("levelCompleted")
--- Player/Stop.gd
+++ Player/Stop.gd
@@ -1,4 +1,7 @@
 extends State
 
+@export var walk_audio : AudioStreamPlayer2D
+
 func on_enter():
+	walk_audio.stop()
 	pass
