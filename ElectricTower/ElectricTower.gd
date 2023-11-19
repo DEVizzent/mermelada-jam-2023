c4b7846f1e553c09a440eb4294db772539e972df
cardiako <cardiak@gmail.com>
2023-11-19 21:46:38 +0100

    Creado Nivel 4, modificado la Escena player dentro de la función electric_damage

--- ElectricTower/ElectricTower.gd
+++ ElectricTower/ElectricTower.gd
@@ -15,7 +15,10 @@ func _ready():
 
 
 func _on_electric_tower_5_sections_body_entered(body):
-	if body.has_method('electric_damage') and $ElectricTower5Sections/SparkDamage.is_playing():
+	if body.is_in_group("Player"):
+		print("entra")
+		
+	if body.has_method('electric_Damage'):
 		body.electric_damage()
 	
 
@@ -28,64 +31,32 @@ func _on_electric_tower_3_sections_body_entered(body):
 
 func _on_timer_timeout():
 	speed += 0.2
-	print(speed)
-	match type:
-		"3Sections":
-			if sparkDamage == true:
-				$ElectricTower3Sections/SparksBegining1Section.set_speed_scale(speed)
-				$ElectricTower3Sections/SparksBegining2Section.set_speed_scale(speed)
-				$ElectricTower3Sections/SparksBegining1Section.show()
-				$ElectricTower3Sections/SparksBegining2Section.show()
-				sparkDamage = false
-				$Timer.set_wait_time(0.5)
-		
-			if speed <= 2:
-				$ElectricTower3Sections/SparksBegining1Section.set_speed_scale(speed)
-				$ElectricTower3Sections/SparksBegining2Section.set_speed_scale(speed)
-			
-			if speed >= 2:
-				$ElectricTower3Sections/SparksBegining1Section.hide()
-				$ElectricTower3Sections/SparksBegining2Section.hide()
-				$ElectricTower3Sections/SparkDamage.show()
-				$ElectricTower3Sections/SparkDamage.play()
-				
-		"5Sections":
-			if sparkDamage == true:
-				$ElectricTower5Sections/SparksBegining1Section.set_speed_scale(speed)
-				$ElectricTower5Sections/SparksBegining2Section.set_speed_scale(speed)
-				$ElectricTower5Sections/SparksBegining3Section.set_speed_scale(speed)
-				$ElectricTower5Sections/SparksBegining1Section.show()
-				$ElectricTower5Sections/SparksBegining2Section.show()
-				$ElectricTower5Sections/SparksBegining3Section.show()
-				sparkDamage = false
+	if sparkDamage == true:
+		$ElectricTower5Sections/SparksBegining1Section.set_speed_scale(speed)
+		$ElectricTower5Sections/SparksBegining2Section.set_speed_scale(speed)
+		$ElectricTower5Sections/SparksBegining3Section.set_speed_scale(speed)
+		$ElectricTower5Sections/SparksBegining1Section.show()
+		$ElectricTower5Sections/SparksBegining2Section.show()
+		$ElectricTower5Sections/SparksBegining3Section.show()
+		sparkDamage = false
 				
 		
-			if speed <= 2:
-				$ElectricTower5Sections/SparksBegining1Section.set_speed_scale(speed)
-				$ElectricTower5Sections/SparksBegining2Section.set_speed_scale(speed)
-				$ElectricTower5Sections/SparksBegining3Section.set_speed_scale(speed)
-			
-			if speed >= 2:
-				$ElectricTower5Sections/SparksBegining1Section.hide()
-				$ElectricTower5Sections/SparksBegining2Section.hide()
-				$ElectricTower5Sections/SparksBegining3Section.hide()
-				$ElectricTower5Sections/SparkDamage.show()
-				$ElectricTower5Sections/SparkDamage2.show()
-				$ElectricTower5Sections/SparkDamage.play()
-				$ElectricTower5Sections/SparkDamage2.play()
+	if speed <= 1.2:
+		$ElectricTower5Sections/SparksBegining1Section.set_speed_scale(speed)
+		$ElectricTower5Sections/SparksBegining2Section.set_speed_scale(speed)
+		$ElectricTower5Sections/SparksBegining3Section.set_speed_scale(speed)
+	
+	else:
+		$ElectricTower5Sections/SparksBegining1Section.hide()
+		$ElectricTower5Sections/SparksBegining2Section.hide()
+		$ElectricTower5Sections/SparksBegining3Section.hide()
+		$ElectricTower5Sections/SparkDamage.show()
+		$ElectricTower5Sections/SparkDamage2.show()
+		$ElectricTower5Sections/SparkDamage.play()
+		$ElectricTower5Sections/SparkDamage2.play()
 		
 		
 	pass # Replace with function body.
-
-
-func _on_spark_damage_animation_finished():
-	sparkDamage = true
-	$ElectricTower3Sections/SparkDamage.hide()
-	speed = 0.2
-	
-	
-	pass # Replace with function body.
-
 
 func _on_spark_damage_2_animation_finished():
 	sparkDamage = true
